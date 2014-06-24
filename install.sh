800963a3879eca2bae8e74b8b6705b202095a615
Brendan Tobolaski <brendan@tobolaski.com>
2014-06-24 11:51:57 -0400

    Add a very barebones vimrc file
    Nothing quite like starting intentionally from scratch.

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/slate ~/.slate
+ln -s ~/.dotfiles/vimrc ~/.vimrc
