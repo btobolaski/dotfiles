29a924cba41155a885c134c7f2a473e5b6195ff6
Brendan Tobolaski <brendan@tobolaski.com>
2014-06-24 12:40:39 -0400

    Add a base tmux config

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/slate ~/.slate
 ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
