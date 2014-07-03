e71d61b6b96b841b768d2e339f2c908b40cae04c
Brendan Tobolaski <brendan@tobolaski.com>
2014-07-03 10:11:57 -0500

    Setup teamocil to work properly

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/slate ~/.slate
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/teamocil ~/.teamocil
 mkdir -p ~/.ssh/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
