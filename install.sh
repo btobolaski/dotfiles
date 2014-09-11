87a68c6d6d4b4cdb8bb8f24b8b66b51ea557eeb7
Brendan Tobolaski <brendan@tobolaski.com>
2014-09-11 00:55:57 -0500

    Add zsh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+xcode-select --install
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 ln -s ~/.dotfiles/bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/bashrc ~/.bashrc
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
@@ -6,5 +8,8 @@ ln -s ~/.dotfiles/slate ~/.slate
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/teamocil ~/.teamocil
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+chsh -s /usr/local/bin/zsh
 mkdir -p ~/.ssh/bundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
